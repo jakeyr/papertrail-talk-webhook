a8fae9dcca7583591bfaae3364b2c67030b68870
Jacob Rosenberg <jake@lendup.com>
2016-09-20 13:27:11 -0700

    updated for librato support

--- app.rb
+++ app.rb
@@ -8,17 +8,32 @@ require 'pg'
 SPEAK_URL = ENV['SPEAK_URL'] || "http://localhost:7388/speak"
 
 def speak(message)
-  return lambda { Unirest.post "#{SPEAK_URL}/speak",
-                        headers:{ "Content-Type" => "application/json" },
-                        parameters: {message: message}.to_json
-  }
+  Unirest.post "#{SPEAK_URL}/speak",
+    headers:{ "Content-Type" => "application/json" },
+    parameters: {message: message}.to_json
 end
 
 def play(url)
-  return lambda { Unirest.post "#{SPEAK_URL}/play",
-                        headers:{ "Content-Type" => "application/json" },
-                        parameters: {url: url}.to_json
-  }
+  Unirest.post "#{SPEAK_URL}/play",
+    headers:{ "Content-Type" => "application/json" },
+    parameters: {url: url}.to_json
+end
+
+def act(id)
+  uri = URI.parse(ENV['DATABASE_URL'])
+
+  conn = PG::Connection.open :host     => uri.host,
+                             :user     => uri.user,
+                             :password => uri.password,
+                             :port     => uri.port || 5432,
+                             :dbname   => uri.path[1..-1]
+
+  puts "Request going to #{SPEAK_URL}"
+  puts "Taking action for alert #{id}"
+
+  res = conn.exec_params('SELECT value,action FROM pt_id_map WHERE pt_id = $1', [id])
+
+  response = send(res[0]['action'],res[0]['value'])
 end
 
 module PapertrailSpeakWebhook
@@ -27,28 +42,17 @@ module PapertrailSpeakWebhook
       "200\n"
     end
 
-    post '/submit' do
+    post '/submit/papertrail' do
       payload = HashWithIndifferentAccess.new(Yajl::Parser.parse(params[:payload]))
-
-      uri = URI.parse(ENV['DATABASE_URL'])
-
-      conn = PG::Connection.open :host     => uri.host,
-                                 :user     => uri.user,
-                                 :password => uri.password,
-                                 :port     => uri.port || 5432,
-                                 :dbname   => uri.path[1..-1]
-
-      res = conn.exec_params('SELECT value,action FROM pt_id_map WHERE pt_id = $1', [payload['saved_search']['id']])
-
-      puts "Request going to #{SPEAK_URL}"
-
-      puts payload['saved_search']['id']
-
-      response = send(res[0]['action'],res[0]['value']).call()
-      
-      puts response.body
-
+      act(payload['saved_search']['id'])
       'ok'
     end
+
+    post '/submit/librato' do
+      payload = HashWithIndifferentAccess.new(Yajl::Parser.parse(params[:payload]))
+      act(payload['alert']['id'])
+      'ok'
+    end
+
   end
 end
