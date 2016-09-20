219f27012c371b0b5652f0d82ad1721cd6d1e131
Jacob Rosenberg <jake@lendup.com>
2016-09-19 18:50:29 -0700

    adds ability to choose speak or play action per id

--- app.rb
+++ app.rb
@@ -6,6 +6,25 @@ require 'active_support'
 
 SPEAK_URL = ENV['SPEAK_URL'] || "http://localhost:7388/speak"
 
+def speak(message)
+  return lambda { Unirest.post "#{SPEAK_URL}/speak",
+                        headers:{ "Content-Type" => "application/json" },
+                        parameters: {message: message}.to_json
+  }
+end
+
+def play(url)
+  return lambda { Unirest.post "#{SPEAK_URL}/play",
+                        headers:{ "Content-Type" => "application/json" },
+                        parameters: {url: url}.to_json
+  }
+end
+
+$ACTION_MAP = {
+  13445233 => speak("New A M S loan has been boarded")
+  #13445233 => play("http://www.moviesoundclips.net/download.php?id=1628&ft=mp3")
+}
+
 module PapertrailSpeakWebhook
   class App < Sinatra::Base
     get '/' do
@@ -17,9 +36,9 @@ module PapertrailSpeakWebhook
 
       puts "Request going to #{SPEAK_URL}"
 
-      response = Unirest.post SPEAK_URL, 
-                        headers:{ "Content-Type" => "application/json" }, 
-                        parameters: {message: 'New A M S loan has been boarded.'}.to_json
+      puts payload['saved_search']['id']
+      puts  $ACTION_MAP[payload['saved_search']['id']]
+      response = $ACTION_MAP[payload['saved_search']['id']].call()
 
       puts response.body
 
